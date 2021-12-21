1200212150f711309356c67f20baaa0645ff861c
Ken Reibman <89096442+lmaoken@users.noreply.github.com>
2021-12-21 10:32:05 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ const renderCards = (clickedOption) => {
         <div class="activity__info">
           <header class="activity__header">
             <h2 class="activity__name">${name}</h2>
-            <button class="activity__options-ellipsis" aria-label="options">
+            <button class="activity__options-ellipsis" aria-label="Options">
               <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/>
               </svg>
